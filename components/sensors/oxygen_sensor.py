c5868b7fc279d75f2687205543465cea43367daa
Adrian Lino <20170837@aloe.ulima.edu.pe>
2022-11-30 21:13:52 -0500

    Avance

--- components/sensors/oxygen_sensor.py
+++ components/sensors/oxygen_sensor.py
@@ -1,13 +1,27 @@
 from components.sensor import Sensor
+from machine import UART
 
 class OxygenSensor(Sensor):
     
     def __init__(self, id):
         super().__init__(id)
+        self.serial = UART(1, 115200)
         
-
     def read(self):
         '''Lee el valor del sensor'''
-        pass
+        _awake()
+        self.serial.write("R")
+        self.serial.write('\r')
+        response = self.serial.read()
+        _sleep()
+        return response
 
-    
+    def _awake(self):
+        self.serial.write("K")
+        self.serial.write('\r')
+        return self.serial.read()
+
+    def _sleep(self):
+        self.serial.write("SLEEP")
+        self.serial.write('\r')
+        return self.serial.read()
